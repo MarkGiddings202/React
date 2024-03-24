d1d2ab392ce5793806143d8def8957b23552a61f
MarkGiddings202 <gavingiddings1997@gmail.com>
2024-03-24 12:21:14 -0400

    events setup

--- practice-app/src/App.js
+++ practice-app/src/App.js
@@ -1,13 +1,19 @@
 import React from "react";
-import Warmup from "./Stateless-Components/Warmup";
-import ExerciseOne from "./Stateless-Components/Exercise-1";
-import ExerciseTwo from "./Stateless-Components/Exercise-2";
-import ExerciseThree from "./Stateless-Components/Exercise-3";
+// import Warmup from "./Stateless-Components/Warmup";
+// import ExerciseOne from "./Stateless-Components/Exercise-1";
+// import ExerciseTwo from "./Stateless-Components/Exercise-2";
+// import ExerciseThree from "./Stateless-Components/Exercise-3";
+import EventsWarmup from "./Event/Warmup(events)";
+import EventsExerciseOne from "./Event/Exercise-1(events)";
+import EventsExerciseTwo from "./Event/Exercise-2(events)";
+import EventsExerciseThree from "./Event/Exercise-3(events)";
 
 function App() {
   return (
     <>
     <div>
+    {/* 
+    Stateless Components 
       <Warmup />
       <hr></hr>
       <ExerciseOne />
@@ -15,8 +21,16 @@ function App() {
       <ExerciseTwo />
       <hr></hr>
       <ExerciseThree />
-     
+    */}
+    {/* Events */}
+    <EventsWarmup/>
+    <EventsExerciseOne/>
+    <EventsExerciseTwo/>
+    <EventsExerciseThree/>
+    
+
     </div>
+
     </>
   );
 }
